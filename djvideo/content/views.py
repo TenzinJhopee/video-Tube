134d339ebfb17b092064ec2c6a1aa524ff741d36
Tenzin Sean <tenzin.jeunesse@gmail.com>
2022-09-07 06:17:38 +0200

    add course list and detail

--- djvideo/content/views.py
+++ djvideo/content/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Course
+
+
 # Create your views here.
 class CourseListView(generic.ListView):
     template_name = 'content/course_list.html'
+    queryset = Course.objects.all()
+
+
+class CourseDetailView(generic.DetailView):
+    template_name = 'content/course_detail.html'
     queryset = Course.objects.all()
